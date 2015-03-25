fdb82180f0f9cc56fb673bd753d0029d7d78da85
n1kropol <n1kropol@yandex.ua>
2015-03-25 19:55:36 +0200

    first main java from git

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+            // write your code here
+            //System.out.print(12 + 20);
+            //int x = 12;
+            // x = 32.2;
+            System.out.println("Sum of 256 and 3180: "+ (256 + 3180));
+            System.out.println("Sum of 180 / 30 : "+ 180 / 30);
+/*            boolean a;
+            boolean b;
+
+            int[] h1={10,0,0,21,-5,38};
+            System.out.println(h1);
+            System.out.println(h1[0]+","+h1[1]+","+h1[2]+","+h1[3]+","+h1[4]+","+h1[5]);
+            System.out.println(args[0]);
+            System.out.println(args[1]);
+
+            String[] ar={"a","b","c"};
+            String[] ar2={"a","d","c"};
+
+            System.out.println(ar[0]+"-"+ar2[0]+","+ar[1]+"-"+ar2[1]+","+ar[2]+"-"+ar2[2]);*/
+
+
+
+    }
+}
