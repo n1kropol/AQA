525b0c18ae4a643f198de3c95cf89df5a1efff49
n1kropol <n1kropol@yandex.ua>
2015-03-25 21:03:54 +0200

    first main java from git

---
+++ src/com/company/Main2.java
@@ -0,0 +1,30 @@
+package com.company;
+
+/**
+ * Created by Admin on 25.03.15.
+ */
+public class Main2 {
+    public static void main(String[] args) {
+        //System.out.println(args[0]);
+//////////////////////// task 1/////////////////////////////////
+        if (args[0].equals("Hello") )
+        //if (args[0] == "Hello")
+            System.out.println("Class");
+        else
+            System.out.println("Good bye");
+//////////////////////// task 2/////////////////////////////////
+        if (args[0].equals("Hello") && args[1].equals("Class"))
+            System.out.println("Success");
+        else
+            System.out.println("Good bye");
+
+        if()
+            if()
+                if()
+                    System.out.println();
+
+    }
+}
+
+
+
