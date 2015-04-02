912431b586d7f8142c7bc9720a327e2e901301d7
n1kropol <n1kropol@yandex.ua>
2015-04-02 17:28:23 +0300

    created class by task

---
+++ src/com/homeWorks/5/Stakan.java
@@ -0,0 +1,39 @@
+/**
+ * Created by n1kropol on 02.04.2015.
+ */
+public class Stakan {
+    public String imia;
+    public static String material = "Bumaga";
+    public static int vysota = 10;
+    public int realCapacity = 0;
+
+    // contructor
+    public Stakan(String name){
+        this.imia = name;
+    }
+
+    public static int obiom(int capacity){
+        return capacity * 20;
+    }
+
+    public int procentZapolneniia(){
+        return this.realCapacity;
+    }
+
+    public void nalit(int add){
+        if((this.realCapacity + add) < 100)
+            this.realCapacity += add;
+        else
+            this.realCapacity = 100;
+    }
+
+    public int vylit(){
+        int vylit = realCapacity;
+        this.realCapacity = 0;
+        return vylit;
+    }
+
+    public String imia(){
+        return imia;
+    }
+}
