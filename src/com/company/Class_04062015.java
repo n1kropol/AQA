564a1796ed41e1e3b9fce21de86fe13b6bcbeeb3
n1kropol <n1kropol@yandex.ua>
2015-04-08 20:40:16 +0300

    Seven lesson (WebDriver, http://angel.net/, JUnit)

--- src/com/company/Class_04062015.java
+++ src/com/company/Class_04062015.java
@@ -1,37 +1,129 @@
 package com.company;
 
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
 /**
  * Created by Admin on 06.04.15.
  */
+@RunWith(JUnit4.class)
 public class Class_04062015 {
-    public static void main(String[] args) {
+    public WebDriver h;
+
+    public static WebDriver getConnect(){
+        System.setProperty("webdriver.chrome.driver",
+                "C:/Documents and Settings/Admin/QA/QASource/QA/chromedriver_win32/chromedriver.exe");
+
+         return new ChromeDriver();
+
+
+    }
+
+    @Test
+    public void test1(){
+
+
+        WebElement master = h.findElement(By.name("master"));
+        master.sendKeys("12345678");
+        master.sendKeys(Keys.ENTER);
+
+        WebElement site  = h.findElement(By.name("site"));
+        site.sendKeys("gmail.com");
+        site.sendKeys(Keys.ENTER);
+
+        String pas = h.findElement(By.name("password")).getAttribute("value");
+        Assert.assertEquals("Test failed!!!","W3Hdka0clbEI+@1a", pas);
+        //h.quit();
+    }
+
+    @Before
+    public void doBefore(){
+        System.setProperty("webdriver.chrome.driver",
+                "C:/Documents and Settings/Admin/QA/QASource/QA/chromedriver_win32/chromedriver.exe");
+
+        h = new ChromeDriver();
+        h.get("http://angel.net/~nic/passwd.current.html");
+    }
+
+    @After
+    public void clean(){
+        h.quit();
+    }
+
+    @Test
+    public void test2(){
+/*        System.setProperty("webdriver.chrome.driver",
+                "C:/Documents and Settings/Admin/QA/QASource/QA/chromedriver_win32/chromedriver.exe");
+
+        h = new ChromeDriver();
+        h.get("http://angel.net/~nic/passwd.current.html");*/
+
+        WebElement master = h.findElement(By.name("master"));
+        master.sendKeys("");
+        master.sendKeys(Keys.ENTER);
+
+        WebElement site  = h.findElement(By.name("site"));
+        site.sendKeys("gmail.com");
+        site.sendKeys(Keys.ENTER);
+
+        String pas = h.findElement(By.name("password")).getAttribute("value");
+        Assert.assertEquals("Test failed!!!","zmcHOAyf2oZm+@1a", pas);
+        //h.quit();
+    }
+
+    @Test
+    public void test3(){
+/*        System.setProperty("webdriver.chrome.driver",
+                "C:/Documents and Settings/Admin/QA/QASource/QA/chromedriver_win32/chromedriver.exe");
+
+        h = new ChromeDriver();
+        h.get("http://angel.net/~nic/passwd.current.html");*/
+
+        WebElement master = h.findElement(By.name("master"));
+        master.sendKeys("12345678");
+        master.sendKeys(Keys.ENTER);
+
+        WebElement site  = h.findElement(By.name("site"));
+        site.sendKeys("");
+        site.sendKeys(Keys.ENTER);
+
+        String pas = h.findElement(By.name("password")).getAttribute("value");
+        Assert.assertEquals("Test failed!!!","9Ixm2r5Xnm41Q@1a", pas);
+        //h.quit();
+    }
+    /*public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver",
                 "C:/Documents and Settings/Admin/QA/QASource/QA/chromedriver_win32/chromedriver.exe");
 
         WebDriver h = new ChromeDriver();
-        h.manage().window().maximize();
+        //h.manage().window().maximize();
         h.get("http://angel.net/~nic/passwd.current.html");
         //System.out.println(h.getTitle()); // получить заголовок
         h.findElement(By.name("master")).clear();
 
-        System.out.println(checkPassword(getWebDriver(h, "123", "vk.com"), "PnNKCJ2BVg2Zm@1a"));
-        System.out.println(checkPassword(getWebDriver(h, "123", "vk.com"), "5XEM6FFR+M1qu@1a"));
-        System.out.println(checkPassword(getWebDriver(h, "321", "vk.com"), "fxjzWYJM93SSf@1a"));
+        System.out.println(checkPassword(test1(h, "123", "vk.com"), "PnNKCJ2BVg2Zm@1a"));
+//        System.out.println(checkPassword(getWebDriver(h, "123", "vk.com"), "5XEM6FFR+M1qu@1a"));
+//        System.out.println(checkPassword(getWebDriver(h, "321", "vk.com"), "fxjzWYJM93SSf@1a"));
         //System.out.println(checkPassword(getWebDriver(h, "123", "vk.com"), "Hello"));
         //System.out.println(checkPassword(getWebDriver(h, "123321", "vk.com"), getWebDriver(h, "123321", "vk.com")));
         //System.out.println(getWebDriver(h, "123", "skype.com"));
         //System.out.println(getWebDriver(h, "123", "gmail.com"));
 
 
+
         h.quit();
 
-/*        WebElement master = h.findElement(By.name("master"));
+        WebElement master = h.findElement(By.name("master"));
         master.sendKeys("12345");
         master.sendKeys(Keys.ENTER);
 
@@ -39,7 +131,7 @@ public class Class_04062015 {
         site.sendKeys("vk.com");
         site.sendKeys(Keys.ENTER);
 
-        System.out.println(h.findElement(By.name("password")).getAttribute("value"));*/
+        System.out.println(h.findElement(By.name("password")).getAttribute("value"));*//**//*
 
 
     }
@@ -67,5 +159,5 @@ public class Class_04062015 {
         site.sendKeys(Keys.ENTER);
         return driver.findElement(By.name("password")).getAttribute("value");
     }
-
+*/
 }
---
+++ src/com/company/UTest.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Created by Admin on 08.04.15.
+ */
+@RunWith(JUnit4.class)
+public class UTest {
+
+    @Test
+    public void test1(){
+       //Assert.fail();
+    }
+
+    @Test
+    public void test2(){
+        int a = 10;
+        int b =5;
+        int expectedResult = 16;
+        Assert.assertEquals("Test failed!!!",expectedResult, a + b);
+        Assert.assertTrue("Test failed!!!", expectedResult == (a + b));
+    }
+}
