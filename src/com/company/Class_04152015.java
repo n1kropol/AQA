abe6ab610cb18cc72ebf59f8c0234768a01d5ac9
n1kropol <n1kropol@yandex.ua>
2015-04-22 20:39:54 +0300

    Nineth lesson (WebDriver, http://angel.net/, JUnit, Xpath)

--- src/com/company/Class_04152015.java
+++ src/com/company/Class_04152015.java
@@ -6,10 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.ArrayList;
@@ -23,52 +20,52 @@ public class Class_04152015 {
     public static WebDriver h;
     @Before
     public void doBefore(){
-        System.setProperty("webdriver.chrome.driver",
-                "C:/Documents and Settings/Admin/QA/QASource/QA/chromedriver_win32/chromedriver.exe");
+        h = POGenPass.init();
+//        System.setProperty("webdriver.chrome.driver",
+//                "C:/Documents and Settings/Admin/QA/QASource/QA/chromedriver_win32/chromedriver.exe");
 
-        h = new ChromeDriver();
+//        h = new ChromeDriver();
         //h.get("http://angel.net/~nic/passwd.current.html");
         //h.get("http://oxogamestudio.com/passwd.current2.htm");
         //h.get("http://oxogamestudio.com/passwd.current3.htm");
         //h.get("http://oxogamestudio.com/passwd.current4.htm");
         //h.get("http://oxogamestudio.com/passwd.current5.htm");
         //h.get("http://oxogamestudio.com/passwd.current6.htm");
-        h.get("http://oxogamestudio.com/passwd.current7.htm");
-
-
+        //h.get("http://oxogamestudio.com/passwd.current7.htm");
+//        h.get("http://oxogamestudio.com/passwd.current7.htm");
+//        h.get("http://oxogamestudio.com/passwd.current8.htm");
+//        h.get("http://oxogamestudio.com/passwd.current9.htm");
     }
     @Test
     public void test1(){
-        setMaster("12345678");
-        setSite("gmail.com");
-        generate();
-        String pas = getPassword();
+        POGenPass.setMaster("12345678");
+        POGenPass.setSite("gmail.com");
+        POGenPass.generate();
+        String pas = POGenPass.getPassword();
 
         Assert.assertEquals("Test failed!!!", "W3Hdka0clbEI+@1a", pas);
 
     }
     @Test
     public void test2(){
-        setMaster("");
-        setSite("gmail.com");
-        generate();
-        String pas = getPassword();
+        POGenPass.setMaster("");
+        POGenPass.setSite("gmail.com");
+        POGenPass.generate();
+        String pas = POGenPass.getPassword();
         Assert.assertEquals("Test failed!!!","zmcHOAyf2oZm+@1a", pas);
     }
     @Test
     public void test3(){
-        setMaster("12345678");
-        setSite("");
-        generate();
-        String pas = getPassword();
+        POGenPass.setMaster("12345678");
+        POGenPass.setSite("");
+        POGenPass.generate();
+        String pas = POGenPass.getPassword();
         Assert.assertEquals("Test failed!!!","9Ixm2r5Xnm41Q@1a", pas);
     }
     @Test
     public void test4(){
-        List<WebElement> list = new ArrayList<WebElement>();
-        list = h.findElements(By.tagName("input"));
-        list.get(2).click();
-        String pas = getPassword();
+        POGenPass.generate();
+        String pas = POGenPass.getPassword();
         Assert.assertEquals("Test failed!!!","BaefBs8/Z/cm2@1a", pas);
     }
     @Test
@@ -77,81 +74,70 @@ public class Class_04152015 {
         for (int i = 0; i < 200; i++) {
             str = str + 1;
         }
-        setMaster(str);
-        setSite(str);
-        generate();
-        String pas = getPassword();
+        POGenPass.setMaster(str);
+        POGenPass.setSite(str);
+        POGenPass.generate();
+        String pas = POGenPass.getPassword();
         Assert.assertEquals("Test failed!!!","aR8ztwNBbSqe5@1a", pas);
     }
     @Test
     public void test6(){
-
-        setMaster("/';*&#&$@^!_...\\n");
-        setSite("/';*&#&$@^!_...\\n");
-        generate();
-        String pas = getPassword();
+        POGenPass.setMaster("/';*&#&$@^!_...\\n");
+        POGenPass.setSite("/';*&#&$@^!_...\\n");
+        POGenPass.generate();
+        String pas = POGenPass.getPassword();
         Assert.assertEquals("Test failed!!!","ctolW6AdI0te1@1a", pas);
     }
     @Test
     public void test7(){
-        //List<WebElement> list = new ArrayList<WebElement>(h.findElements(By.tagName("input")));
         WebElement td = h.findElement(By.xpath("//input[@value='Generate']"));
-        //String title = list.get(2).getAttribute("value");
         String title = td.getAttribute("value");
         Assert.assertEquals("Test failed!!!","Generate", title);
     }
     @Test
     public void test8(){
-        setMaster("/';*&#&$@^!_...\\n");
-        setSite("/';*&#&$@^!_...\\n");
-        generate();
-        //    //td[text()='Site name']/following::input
+        POGenPass.setMaster("/';*&#&$@^!_...\\n");
+        POGenPass.setSite("/';*&#&$@^!_...\\n");
+        POGenPass.generate();
         Assert.assertEquals("/';*&#&$@^!_...\\n", h.findElement(By.xpath("//td[text()='Your master password']/following::input")).getAttribute("value"));
     }
     @Test
     public void test9(){
-        setMaster("/';*&#&$@^!_...\\n");
-        setSite("/';*&#&$@^!_...\\n");
-        generate();
-        //    //td[text()='Your master password']/following::input
+        POGenPass.setMaster("/';*&#&$@^!_...\\n");
+        POGenPass.setSite("/';*&#&$@^!_...\\n");
+        POGenPass.generate();
         Assert.assertEquals("Master is not editable",true, h.findElement(By.xpath("//td[text()='Your master password']/following::input")).isEnabled());
     }
     @Test
     public void test10(){
-        setMaster("12345678");
-        setSite("gmail.com");
-        generate();
-        //   //td[text()='Your master password']/following::input
+        POGenPass.setMaster("12345678");
+        POGenPass.setSite("gmail.com");
+        POGenPass.generate();
         Assert.assertEquals("Site is not editable",true, h.findElement(By.xpath("//td[text()='Site name']/following::input")).isEnabled());
     }
     @Test
     public void test11(){
-        setMaster("12345678");
-        setSite("gmail.com");
-        generate();
+        POGenPass.setMaster("12345678");
+        POGenPass.setSite("gmail.com");
+        POGenPass.generate();
         Assert.assertEquals("Password is not editable",true, h.findElement(By.xpath("//td[text()='Generated password']/following::input")).isEnabled());
     }
     @Test
     public void test12(){
-        //List<WebElement> list = new ArrayList<WebElement>(h.findElements(By.tagName("td")));
-        //List<WebElement> list = new ArrayList<WebElement>(h.findElements(By.xpath("td[text()='Your master password']")));
         WebElement td = h.findElement(By.xpath("//td[text()='Your master password']"));
         String title = td.getText();
         Assert.assertEquals("Test failed!!!","Your master password", title);
     }
     @Test
     public void test13(){
-        //List<WebElement> list = new ArrayList<WebElement>(h.findElements(By.xpath("td[text()='Site name']")));
         WebElement td = h.findElement(By.xpath("//td[text()='Site name']"));
         String title = td.getText();
         Assert.assertEquals("Test failed!!!","Site name", title);
     }
     @Test
     public void test14(){
-        //List<WebElement> list = new ArrayList<WebElement>(h.findElements(By.xpath("td[text()='Your master password']")));
         WebElement td = h.findElement(By.xpath("//td[text()='Generated password']"));
         String title = td.getText();
-        //String title = list.get(5).getText();
         Assert.assertEquals("Test failed!!!","Generated password", title);
     }
 
@@ -159,39 +145,12 @@ public class Class_04152015 {
     public void clean(){
        h.quit();
     }
-
-    public void setMaster(String mast){
-        //WebElement master = h.findElement(By.name("master"));
-        ////td[text()='Your master password']/following::input
-        WebElement master = h.findElement(By.xpath("//td[text()='Your master password']/following::input"));
-        master.clear();
-        master.sendKeys(mast);
-        //master.sendKeys(Keys.ENTER);
+    @Test
+    public void test15() {
+        POGenPass.setField("Your master password", "12345678");
+        POGenPass.setField("Site name", "gmail.com");
+        POGenPass.generate();
+        String pwd = POGenPass.getField("Generated password");
+        Assert.assertEquals("W3Hdka0clbEI+@1a", pwd);
     }
-    public void setSite(String siteStr){
-        //WebElement site = h.findElement(By.name("site"));
-        ////td[text()='Your master password']/following::input[2]
-        WebElement site = h.findElement(By.xpath("//td[text()='Site name']/following::input"));
-        site.clear();
-        site.sendKeys(siteStr);
-       // site.sendKeys(Keys.ENTER);
-    }
-    public void generate(){
-        //List<WebElement> list = new ArrayList<WebElement>(h.findElements(By.tagName("input")));
-        WebElement button = h.findElement(By.xpath("//input[@value='Generate']"));
-        //list.get(2).click();
-        button.click();
-        //h.findElement(By.xpath("//input[@value='Generate']"));
-    }
-    public String getPassword() {
-        //String pas = h.findElement(By.name("password")).getAttribute("value");
-        String pas = h.findElement(By.xpath("//td[text()='Generated password']/following::input")).getAttribute("value");
-        try {
-            while (h.findElement(By.xpath("//td[text()='Generated password']/following::input")).getAttribute("value").isEmpty()) {
-                Thread.sleep(3000);
-            }
-        } catch(InterruptedException e){}
-        return h.findElement(By.xpath("//td[text()='Generated password']/following::input")).getAttribute("value");
-    }
-
 }
