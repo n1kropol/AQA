94c9da44e0fbe14de8cf0d290791a52c0e4b374b
n1kropol <n1kropol@yandex.ua>
2015-05-12 22:26:30 +0300

    add TestHelperPass for angle test CI dron.io

---
+++ src/com/company/TestHelperPassw.java
@@ -0,0 +1,29 @@
+package com.company;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+/**
+ * Created by n1kropol on 29.04.2015.
+ */
+public class TestHelper {
+    public static WebDriver driver;
+
+    public static void init() {
+//        /usr/local/bin/chromedriver
+      System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+
+        driver = new ChromeDriver();
+        driver.get("http://angel.net/~nic/passwd.current.html");
+        //driver.get("http://oxogamestudio.com/passwd.current2.htm");
+        //driver.get("http://oxogamestudio.com/passwd.current3.htm");
+        //driver.get("http://oxogamestudio.com/passwd.current4.htm");
+        //driver.get("http://oxogamestudio.com/passwd.current5.htm");
+        //driver.get("http://oxogamestudio.com/passwd.current6.htm");
+        //driver.get("http://oxogamestudio.com/passwd.current7.htm");
+        //driver.get("http://oxogamestudio.com/passwd.current7.htm");
+        //driver.get("http://oxogamestudio.com/passwd.current8.htm");
+        //driver.get("http://oxogamestudio.com/passwd.current9.htm");
+    }
+}
